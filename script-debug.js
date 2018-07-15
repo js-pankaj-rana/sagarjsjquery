f17f554d5499beb21735fcff81178fcca328cd98
Panakj <pankaj.csdeveloper@gmail.com>
2018-07-15 11:52:38 +0530

    complete object example.
    Signed-off-by: Panakj <pankaj.csdeveloper@gmail.com>

--- script-debug.js
+++ script-debug.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Frameset//EN'
-'http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd'>
-<html xmlns='http://www.w3.org/1999/xhtml'>
-	<head>
-		<title>Example</title>
-		<body>
-			<script type="application/javascript" src="script-debug.js"></script>
-			
-			<!-- <script type="application/javascript">
+
 				// printing *
 				/*
 				**
@@ -51,7 +43,4 @@
 							
 					}
 
-			</script> -->
-
-		</body>
-</html>
+		
